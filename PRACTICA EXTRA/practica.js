d65ce274e1752e03a4a1cb8f52bde67b8764e854
david <daviddcardozo2006@gmail.com>
2026-05-09 17:40:11 -0300

    update

---
+++ PRACTICA EXTRA/practica.js
@@ -0,0 +1,3 @@
+/*Objetivo: Desarrollar un script que itere del 1 al 20. Si el índice es múltiplo de 3, imprime "Fizz". Si es de 5, 
+"Buzz". Si es de ambos, "FizzBuzz". En cualquier otro caso, imprime el número.*/
+
